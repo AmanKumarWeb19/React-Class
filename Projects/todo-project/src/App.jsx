4176449f2b7596c63f63dc378e86e171f1b3d23c
AmanKumarWeb19 <akgupta0524@gmail.com>
2023-12-16 13:57:36 +0530

    add css with bootstrap

--- Projects/todo-project/src/App.jsx
+++ Projects/todo-project/src/App.jsx
@@ -16,6 +16,26 @@ function App() {
             </button>
           </div>
         </div>
+
+        <div className="row">
+          <div className="col-6">Buy Milk</div>
+          <div className="col-4">16-12-2023</div>
+          <div className="col-2">
+            <button type="button" className="btn btn-danger">
+              Delete
+            </button>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-6">Buy car</div>
+          <div className="col-4">18-12-2023</div>
+          <div className="col-2">
+            <button type="button" className="btn btn-danger">
+              Delete
+            </button>
+          </div>
+        </div>
       </div>
     </center>
   );
