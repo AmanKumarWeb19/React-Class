cbf540815e7cab5ff29858e9465bd1f227e6279b
AmanKumarWeb19 <akgupta0524@gmail.com>
2023-12-16 12:25:07 +0530

    practice

--- 2 create react component/create-first-comp/src/App.jsx
+++ 2 create react component/create-first-comp/src/App.jsx
@@ -1,10 +1,11 @@
+import Hello from "./Hello";
 import KgButton from "./KgButton";
 
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>;
-      <KgButton />
+      <h1>This is the Best Course</h1>;
+      <Hello />
     </div>
   );
 }
---
+++ 2 create react component/create-first-comp/src/Hello.jsx
@@ -0,0 +1,6 @@
+function Hello() {
+  let myName = "Aman";
+
+  return <h3>Hello {myName} ! This is the future speaking</h3>;
+}
+export default Hello;
