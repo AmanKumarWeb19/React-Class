56aa451cd43bee6028e9580df735ecf778969938
AmanKumarWeb19 <akgupta0524@gmail.com>
2023-12-18 12:08:44 +0530

    mam

---
+++ Map-Method/chapter-map/src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <>
+      <h1>Healthy Food</h1>
+      <ul className="list-group">
+        <li className="list-group-item">Chapati</li>
+        <li className="list-group-item">Daal</li>
+        <li className="list-group-item">Salad</li>
+        <li className="list-group-item">Meat</li>
+        <li className="list-group-item">Fruits</li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
