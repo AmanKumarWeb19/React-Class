0528c77cc99804d89fa3ffeb73bb0d5d852be094
AmanKumarWeb19 <akgupta0524@gmail.com>
2023-12-18 14:53:08 +0530

    add css

--- 22 Css Module/chapter-css-module/src/App.jsx
+++ 22 Css Module/chapter-css-module/src/App.jsx
@@ -1,10 +1,19 @@
-import { useState } from "react";
-
-import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import FoodItems from "./Components/FoodItems";
+import ErrorMessage from "./Components/ErrorMessage";
+import "./App.css";
 
 function App() {
-  return <>grdrgd</>;
+  // let foodItems = [];
+  let foodItems = ["Dal", "Chapati", "Salad", "Meat", "Fruits"];
+
+  return (
+    <>
+      <h1 className="kg-heading">Healthy Food</h1>
+      <ErrorMessage items={foodItems} />
+      <FoodItems items={foodItems} />
+    </>
+  );
 }
 
 export default App;
