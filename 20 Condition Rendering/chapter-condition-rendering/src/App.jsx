aa2ac077fc375d10cdac9e29f9281ff175a8f7dc
AmanKumarWeb19 <akgupta0524@gmail.com>
2023-12-18 12:46:53 +0530

    ternary condityion

--- 20 Condition Rendering/chapter-condition-rendering/src/App.jsx
+++ 20 Condition Rendering/chapter-condition-rendering/src/App.jsx
@@ -2,10 +2,13 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  let foodItems = ["Dal", "Chapati", "Salad", "Meat", "Fruits"];
+  let foodItems = [];
+  // let foodItems = ["Dal", "Chapati", "Salad", "Meat", "Fruits"];
+
   return (
     <>
       <h1>Healthy Food</h1>
+      {foodItems.length === 0 ? <h3>I am Still hungry</h3> : null}
       <ul className="list-group">
         {foodItems.map((item) => (
           <li key={item} className="list-group-item">
