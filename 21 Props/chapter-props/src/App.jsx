1e01a6876855fd3e4b0e8a1b81dc8af71489991b
AmanKumarWeb19 <akgupta0524@gmail.com>
2023-12-18 14:11:01 +0530

    prop validation

--- 21 Props/chapter-props/src/App.jsx
+++ 21 Props/chapter-props/src/App.jsx
@@ -1,10 +1,17 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import FoodItems from "./Components/FoodItems";
+import ErrorMessage from "./Components/ErrorMessage";
 
 function App() {
+  // let foodItems = [];
+  let foodItems = ["Dal", "Chapati", "Salad", "Meat", "Fruits"];
+
   return (
     <>
-      <h1>Props</h1>
+      <h1>Healthy Food</h1>
+      <ErrorMessage items={foodItems} />
+      <FoodItems items={foodItems} />
     </>
   );
 }
