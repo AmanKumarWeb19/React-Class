47a3cd73d7a434a9104d9778a50b8b6d49bf82dc
AmanKumarWeb19 <akgupta0524@gmail.com>
2023-12-18 10:34:43 +0530

    update in todo1

--- Projects/todo-project/src/components/TodoItem1.jsx
+++ Projects/todo-project/src/components/TodoItem1.jsx
@@ -1,9 +1,11 @@
 const TodoItem1 = () => {
+  let todoName = "Buy Milk";
+  let todoDate = "16-12-2023";
   return (
     <div className="container text-center">
       <div className="row">
-        <div className="col-6">Buy Milk</div>
-        <div className="col-4">16-12-2023</div>
+        <div className="col-6">{todoName}</div>
+        <div className="col-4">{todoDate}</div>
         <div className="col-2">
           <button type="button" className="btn btn-danger">
             Delete
