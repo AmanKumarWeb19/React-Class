5daa04eaf837a343f1cf3bbce6193e265b3b8063
AmanKumarWeb19 <akgupta0524@gmail.com>
2023-12-18 12:20:24 +0530

    add meap

--- Map-Method/chapter-map/src/App.jsx
+++ Map-Method/chapter-map/src/App.jsx
@@ -2,15 +2,14 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  let foodItems = ["Dal", "Chapati", "Salad", "Meat", "Fruits"];
   return (
     <>
       <h1>Healthy Food</h1>
       <ul className="list-group">
-        <li className="list-group-item">Chapati</li>
-        <li className="list-group-item">Daal</li>
-        <li className="list-group-item">Salad</li>
-        <li className="list-group-item">Meat</li>
-        <li className="list-group-item">Fruits</li>
+        {foodItems.map((item) => (
+          <li className="list-group-item">{item}</li>
+        ))}
       </ul>
     </>
   );
