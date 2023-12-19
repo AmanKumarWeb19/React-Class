ab1795247711c2aefead06202a5636251083b81d
AmanKumarWeb19 <akgupta0524@gmail.com>
2023-12-19 10:21:26 +0530

    event handling

--- 23 Passing Children/child-pass/src/Components/Item.jsx
+++ 23 Passing Children/child-pass/src/Components/Item.jsx
@@ -4,6 +4,12 @@ const Item = ({ foodItem }) => {
   return (
     <li className={`${styles["kg-item"]} list-group-item`}>
       <span className={`${styles["kg-span"]}`}>{foodItem}</span>
+      <button
+        className={`${styles.button} btn btn-info `}
+        onClick={() => console.log("buy button on click")}
+      >
+        Buy
+      </button>
     </li>
   );
 };
