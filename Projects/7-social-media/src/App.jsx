2cefe5e756ceaf441fb4ba7fa09f56905ec6ac4b
AmanKumarWeb19 <akgupta0524@gmail.com>
2023-12-20 13:01:35 +0530

    sidebar and form

--- Projects/7-social-media/src/App.jsx
+++ Projects/7-social-media/src/App.jsx
@@ -2,13 +2,19 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
+import SideBar from "./Components/Sidebar";
+import CreatePost from "./Components/CreatePost";
 
 function App() {
   return (
-    <>
-      <Header />
-      <Footer />
-    </>
+    <div className="app-container">
+      <SideBar />
+      <div className="content">
+        <Header />
+        <CreatePost />
+        <Footer />
+      </div>
+    </div>
   );
 }
 
