520220630d3f9542d4e0d2375def5cf51b32762a
AmanKumarWeb19 <akgupta0524@gmail.com>
2023-12-20 07:30:36 +0530

    context api

---
+++ 35-36 context Api/6 todo-project Using State/src/App.jsx
@@ -0,0 +1,35 @@
+import AddTodo from "./components/AddTodo";
+import AppName from "./components/AppName";
+import TodoItem from "./components/TodoItem";
+import "./App.css";
+import TodoItems from "./components/TodoItems";
+import { useState } from "react";
+import WelcomeMessage from "./components/WelcomeMessage";
+
+function App() {
+  const [todoItems, setTodoItems] = useState([]);
+
+  const handleNewItem = (itemName, itemdueDate) => {
+    const newTodoItems = [
+      ...todoItems,
+      { name: itemName, dueDate: itemdueDate },
+    ];
+    setTodoItems(newTodoItems);
+  };
+
+  const handleDeleteItem = (todoItemName) => {
+    const newTodoItems = todoItems.filter((item) => item.name !== todoItemName);
+    setTodoItems(newTodoItems);
+  };
+
+  return (
+    <center className="todo-container">
+      <AppName />
+      <AddTodo onNewItem={handleNewItem} />
+      <WelcomeMessage todoItems={todoItems} />
+      <TodoItems todoItems={todoItems} onDeleteClick={handleDeleteItem} />
+    </center>
+  );
+}
+
+export default App;
---
+++ 35-36 context Api/6 todo-project Using State/src/components/AddTodo.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { IoBagAddOutline } from "react-icons/io5";
+
+const AddTodo = ({ onNewItem }) => {
+  const [todoName, setTodoName] = useState("");
+  const [dueDate, setDueDate] = useState("");
+
+  const handleNameChange = (event) => {
+    setTodoName(event.target.value);
+  };
+
+  const handleDateChange = (event) => {
+    setDueDate(event.target.value);
+  };
+
+  const handleAddButtonClicked = (event) => {
+    event.preventDefault();
+    onNewItem(todoName, dueDate);
+    setTodoName("");
+    setDueDate("");
+  };
+  return (
+    <div className="container">
+      <form className="row row-class" onSubmit={handleAddButtonClicked}>
+        <div className="col-6">
+          <input
+            type="text"
+            value={todoName}
+            placeholder="Enter Todo Here"
+            onChange={handleNameChange}
+          />
+        </div>
+        <div className="col-4">
+          <input type="date" value={dueDate} onChange={handleDateChange} />
+        </div>
+        <div className="col-2">
+          <button type="submit" className="btn btn-success btn-class">
+            <IoBagAddOutline />
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default AddTodo;
