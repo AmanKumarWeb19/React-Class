4b2dc778f3a911cc47d30fa580b7c9ed90052435
AmanKumarWeb19 <akgupta0524@gmail.com>
2023-12-20 13:43:34 +0530

    add style and function to sidebar

--- Projects/7-social-media/src/App.jsx
+++ Projects/7-social-media/src/App.jsx
@@ -5,15 +5,18 @@ import Footer from "./Components/Footer";
 import SideBar from "./Components/Sidebar";
 import CreatePost from "./Components/CreatePost";
 import PostList from "./Components/PostList";
+import { useState } from "react";
 
 function App() {
+  const [selectedTab, setSelectedTab] = useState("Home");
+
   return (
     <div className="app-container">
-      <SideBar />
+      <SideBar selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
       <div className="content">
         <Header />
-        <CreatePost />
-        <PostList />
+        {selectedTab === "Home" ? <PostList /> : <CreatePost />}
+
         <Footer />
       </div>
     </div>
