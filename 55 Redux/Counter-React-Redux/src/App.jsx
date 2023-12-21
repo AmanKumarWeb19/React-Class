56ebf76feb35a05159313e61fea9acb8f4600878
AmanKumarWeb19 <akgupta0524@gmail.com>
2023-12-21 19:40:29 +0530

    add buttons in store

--- 55 Redux/Counter-React-Redux/src/App.jsx
+++ 55 Redux/Counter-React-Redux/src/App.jsx
@@ -7,17 +7,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <>
-      <center className="px-4 py-5 my-5 text-center">
-        <Container>
-          <Header />
-          <div className="col-lg-6 mx-auto">
-            <DisplayCounter />
-            <Controls />
-          </div>
-        </Container>
-      </center>
-    </>
+    <center className="px-4 py-5 my-5 text-center">
+      <Container>
+        <Header />
+        <div className="col-lg-6 mx-auto">
+          <DisplayCounter />
+          <Controls />
+        </div>
+      </Container>
+    </center>
   );
 }
 
