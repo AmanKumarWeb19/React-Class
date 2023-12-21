44b6af9b4ee39d7f029875bd85e4afc87a9ad06e
AmanKumarWeb19 <akgupta0524@gmail.com>
2023-12-21 14:04:32 +0530

    using loader now

---
+++ 50 Data Fetching using Loader/src/routes/App.jsx
@@ -0,0 +1,31 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "../Components/Header";
+import Footer from "../Components/Footer";
+import SideBar from "../Components/Sidebar";
+import CreatePost from "../Components/CreatePost";
+import PostList from "../Components/PostList";
+import { useState } from "react";
+import PostListProvider from "../store/post-list-stores";
+import { Outlet } from "react-router-dom";
+
+function App() {
+  const [selectedTab, setSelectedTab] = useState("Home");
+
+  return (
+    <PostListProvider>
+      <div className="app-container">
+        <SideBar selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
+        <div className="content">
+          <Header />
+          {/* {selectedTab === "Home" ? <PostList /> : <CreatePost />} */}
+          <Outlet />
+
+          <Footer />
+        </div>
+      </div>
+    </PostListProvider>
+  );
+}
+
+export default App;
