3a25903758f04ea7a77b35f6c1cf1fdf484e05d7
AmanKumarWeb19 <akgupta0524@gmail.com>
2023-12-21 11:25:20 +0530

    update

--- Projects/8 bharat-clock useEffect Method/src/Components/CurrrentTime.jsx
+++ Projects/8 bharat-clock useEffect Method/src/Components/CurrrentTime.jsx
@@ -4,14 +4,11 @@ const CurrentTime = () => {
   const [time, setTime] = useState(new Date());
 
   useEffect(() => {
-    console.log("setthe inteval");
-
     const intervalId = setInterval(() => {
       setTime(new Date());
     }, 1000);
     return () => {
       clearInterval(intervalId);
-      console.log("cancel the inteval");
     };
   }, []);
   return (
