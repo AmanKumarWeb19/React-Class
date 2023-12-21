384fc5289ceb7c6552e65656533251590901f684
AmanKumarWeb19 <akgupta0524@gmail.com>
2023-12-21 20:08:41 +0530

    half app copmlete

--- 55 Redux/Counter-React-Redux/src/components/Controls.jsx
+++ 55 Redux/Counter-React-Redux/src/components/Controls.jsx
@@ -1,11 +1,22 @@
+import { useDispatch } from "react-redux";
+
 const Controls = () => {
+  const dispatch = useDispatch();
+
+  const handleAdd = () => {
+    dispatch({ type: "INCREMENT" });
+  };
+
+  const handleMinus = () => {
+    dispatch({ type: "DECREMENT" });
+  };
   return (
     <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-      <button type="button" className="btn btn-primary">
+      <button type="button" className="btn btn-primary" onClick={handleAdd}>
         +1
       </button>
 
-      <button type="button" className="btn btn-success">
+      <button type="button" className="btn btn-success" onClick={handleMinus}>
         -1
       </button>
     </div>
