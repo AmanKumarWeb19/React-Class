f9732e995febe4b51fbe42140e450008d48bd631
AmanKumarWeb19 <akgupta0524@gmail.com>
2023-12-22 11:22:27 +0530

    redux toolkit

---
+++ 55 Redux/3Z-Counter-React-Redux-ToolKit/src/store/index.js
@@ -0,0 +1,50 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+const counterSlice = createSlice({
+  name: "counter",
+  InitialState: { counterVal: 0 },
+  reducers: {
+    increment: (state, action) => {
+      console.log(state, action);
+    },
+    decrement: (state, action) => {
+      console.log(state, action);
+    },
+    add: (state, action) => {
+      console.log(state, action);
+    },
+    substract: (state, action) => {
+      console.log(state, action);
+    },
+  },
+});
+
+const CounterStore = configureStore({
+  reducer: {
+    counter: counterSlice.reducer,
+  },
+});
+
+export const counterActions = counterSlice.actions;
+export default CounterStore;
+
+// const INITIAL_VALUE = {
+//   counter: 0,
+//   privacy: false,
+// };
+
+// const CounterReducer = (store = INITIAL_VALUE, action) => {
+//   if (action.type === "INCREMENT") {
+//     return { ...store, counter: store.counter + 1 };
+//   } else if (action.type === "DECREMENT") {
+//     return { ...store, counter: store.counter - 1 };
+//   } else if (action.type === "ADD") {
+//     return { ...store, counter: store.counter + Number(action.payload.num) };
+//   } else if (action.type === "SUBTRACT") {
+//     return { ...store, counter: store.counter - Number(action.payload.num) };
+//   } else if (action.type === "PRIVACY_TOGGLE") {
+//     return { ...store, privacy: !store.privacy };
+//   }
+//   console.log("Action Recieved", action);
+//   return store;
+// };
