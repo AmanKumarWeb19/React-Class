fbf816daba198d6f97becee3eb3598ef69e872cc
AmanKumarWeb19 <akgupta0524@gmail.com>
2023-12-22 09:01:59 +0530

    add new method

--- 55 Redux/Counter-React-Redux/src/store/index.js
+++ 55 Redux/Counter-React-Redux/src/store/index.js
@@ -9,6 +9,10 @@ const CounterReducer = (store = INITIAL_VALUE, action) => {
     return { counter: store.counter + 1 };
   } else if (action.type === "DECREMENT") {
     return { counter: store.counter - 1 };
+  } else if (action.type === "ADD") {
+    return { counter: store.counter + Number(action.payload.num) };
+  } else if (action.type === "SUBTRACT") {
+    return { counter: store.counter - Number(action.payload.num) };
   }
   console.log("Action Recieved", action);
   return store;
