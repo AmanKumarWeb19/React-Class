891ebf1788eb140c20ad07b1f2f57be58c7dbc34
AmanKumarWeb19 <akgupta0524@gmail.com>
2023-12-27 10:30:58 +0530

    circle

---
+++ Projects/9 Myntra Clone/3-myntra-react-clone/src/components/Circle.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Circle = () => {
+  return (
+    <div>
+      <img
+        src="https://www.shutterstock.com/image-vector/radial-lines-different-thicknesses-circle-600nw-2278672767.jpg"
+        alt="circle"
+      />
+    </div>
+  );
+};
+
+export default Circle;
